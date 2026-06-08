c9f90cc14b2639b0b31496634cb597e88b1b14ea
Chimdindu Uzowulu <chimdindu73@gmailcom>
2026-06-08 10:18:34 +0100

    build resolve

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import AOS from "aos";
 import NavBar from "../components/navbar/Navbar";
